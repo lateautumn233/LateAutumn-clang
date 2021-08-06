a65fbd25f9f212f3600f4b03bae257d3f4018f38
lateautumn233 <3487467850@qq.com>
2021-08-06 03:36:56 +0000

    Update to 2021080603 build LLVM commit: https://github.com/llvm/llvm-project/commit/9d10be70a8289b50b1df32605516a0060854c91a binutils commit: https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=8179e388b60acc6ac35b40cd154f8d56234d1c3b

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "1577c41090a0606432688a9a9cd1744cddcccbda"
+#define LLVM_REVISION "9d10be70a8289b50b1df32605516a0060854c91a"
 #define LLVM_REPOSITORY "https://github.com/llvm/llvm-project"
